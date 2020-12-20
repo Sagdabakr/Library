e5404352307da8c8dabf7af1bac603a7c09f7c96
Sagda AbuBkr <sagdaabubkr@outlook.com>
2020-12-20 11:23:59 +0200

    FirstCommit

---
+++ LibraryData/Models/LibraryBranch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class LibraryBranch
+    {
+        public int Id{ get; set; }
+        [Required]
+        [StringLength(30,ErrorMessage ="Limit Branch Name to 30 Characters.")]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Telephone { get; set; }
+        public string Description { get; set; }
+        public DateTime OpenDate { get; set; }
+        public IEnumerable<Patron> Patrons { get; set; }
+        public IEnumerable<LibraryAsset> LibraryAssets { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ LibraryData/Models/LibraryCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class LibraryCard
+    {
+        public int Id { get; set; }
+        public int Fees { get; set; }
+        public DateTime Created { get; set; }
+        public IEnumerable<Checkout> Checkouts  { get; set; }
+    }
+}
